4e6334f113fa129ad8a0c7051bae3fcc1f1f8d9d
Katsiaryna Pazniak <Katsiaryna.Pazniak@itechart-group.com>
2022-10-14 14:49:49 +0200

    rsolved conflicts after rebase

--- cypress/e2e/api_testing/gorest_api_delete_tests.cy.js
+++ cypress/e2e/api_testing/gorest_api_delete_tests.cy.js
@@ -1,6 +1,13 @@
+import { API_Functions } from "../../pages/api_functions_page";
+
 describe('WHen I test all the DELETE operations on Gorest User table', () => {
     let userId;
-     
+    const api_functions = new API_Functions();
+    const urlsTable = {
+        usersCorrectEndpoint: 'https://gorest.co.in/public/v2/users',
+        usersIncorrectEndpoint: 'https://gorest.co.in/public/v2/user'
+    }
+
     before(() => {
         cy.createAPIUser('api_gorest')
             .then((data) => {
@@ -9,49 +16,31 @@ describe('WHen I test all the DELETE operations on Gorest User table', () => {
     });
 
     it('Then deleting with no auth token provided throws error', () => {
-        cy.request({
-            method: 'DELETE',
-            failOnStatusCode: false,
-            url: `https://gorest.co.in/public/v2/users/${userId}`,
-        })
-        .then(response => {
-            expect(response.status).to.equal(404);
-        })
+        api_functions.deleteWithNoToken(urlsTable.usersCorrectEndpoint + `/${userId}`)
+                        .then(response => {
+                            expect(response.status).to.equal(404);
+                        });
     });
 
     it('Then deleting with incorrect endpoint throws error', () => {
-        cy.request({
-            method: 'DELETE',
-            failOnStatusCode: false,
-            url: `https://gorest.co.in/public/v2/user/${userId}`,
-        })
-        .then(response => {
-            expect(response.status).to.equal(404);
-        })
+        api_functions.deleteWithNoToken(urlsTable.usersIncorrectEndpoint + `/${userId}`)
+                        .then(response => {
+                            expect(response.status).to.equal(404);
+                        });
     });
 
     it('Then deleting with no user id throws error', () => {
-        cy.request({
-            method: 'DELETE',
-            failOnStatusCode: false,
-            url: `https://gorest.co.in/public/v2/users/`,
-        })
-        .then(response => {
-            expect(response.status).to.equal(404);
-        })
+        api_functions.deleteWithNoToken(urlsTable.usersCorrectEndpoint)
+                        .then(response => {
+                            expect(response.status).to.equal(404);
+                        });
     });
 
     it('Then deleting with all the fields of the created user works correctly', () => {
-        cy.request({
-            method: 'DELETE',
-            auth: {
-                bearer: Cypress.env('bearerToken')
-            },
-            url: `https://gorest.co.in/public/v2/users/${userId}`,
-        })
-        .then(response => {
-            expect(response.status).to.equal(204);
-            expect(response.body).to.be.empty;
-        })
+        api_functions.deleteWithToken(urlsTable.usersCorrectEndpoint + `/${userId}`)
+                        .then(response => {
+                            expect(response.status).to.equal(204);
+                            expect(response.body).to.be.empty;
+                        });
     });
 });
--- cypress/e2e/api_testing/nbp_api_tests.cy.js
+++ cypress/e2e/api_testing/nbp_api_tests.cy.js
@@ -1,114 +1,105 @@
-describe('NBP Web Api Testing', () => {
+import { API_Functions } from "../../pages/api_functions_page";
 
+describe('NBP Web Api Testing', () => {
+    const api_functions = new API_Functions();
+    const urlsTable = {
+        exchangeratesTodayUSD: 'http://api.nbp.pl/api/exchangerates/rates/c/usd/today/?format=json', 
+        exchangeratesTableA: 'http://api.nbp.pl/api/exchangerates/tables/A/?format=json',
+        exchangeratesTableB: 'http://api.nbp.pl/api/exchangerates/rates/B/BRL/?format=json',
+        exchangeratesTableAToday: 'http://api.nbp.pl/api/exchangerates/tables/a/today/',
+        exchangeratesTableAGBP10Days: 'http://api.nbp.pl/api/exchangerates/rates/a/gbp/last/10/?format=json',
+        exchangeratesTableAGBPDate: 'http://api.nbp.pl/api/exchangerates/rates/a/gbp/2012-01-01/2012-01-31/',
+        exchangeratesTableACHF: 'http://api.nbp.pl/api/exchangerates/rates/a/chf/'
+    }
+    
     it('Testing today`s USD exchange rates', () => {
-        cy.request({
-            method: 'GET',
-            url: 'http://api.nbp.pl/api/exchangerates/rates/c/usd/today/?format=json'
-        })
-        .then(response => {
-            expect(response.headers['content-type']).includes('application/json');
-            expect(response.status).to.equal(200);
-            expect(response.body.code).to.equal('USD');
-            expect(response.body.rates.length).to.be.equal(1);
-        });
+        api_functions.getRecordsFromTable(urlsTable.exchangeratesTodayUSD)
+                        .then(response => {
+                            expect(response.headers['content-type']).includes('application/json');
+                            expect(response.status).to.equal(200);
+                            expect(response.body.code).to.equal('USD');
+                            expect(response.body.rates.length).to.be.equal(1);
+                        });
     });
 
     it('Testing Current table of exchange rates of type A', () => {
-        cy.request({
-            method: 'GET',
-            url: 'http://api.nbp.pl/api/exchangerates/tables/A/?format=json'
-        })
-        .then(response => {
-            expect(response.body).not.to.be.empty
-            expect(response.status).to.equal(200);
-            expect(response.body[0].table).to.equal('A');
-            expect(response.body[0].rates.length).to.be.equal(34);
+        api_functions.getRecordsFromTable(urlsTable.exchangeratesTableA)
+                        .then(response => {
+                            expect(response.body).not.to.be.empty
+                            expect(response.status).to.equal(200);
+                            expect(response.body[0].table).to.equal('A');
+                            expect(response.body[0].rates.length).to.be.equal(34);
 
-            let codesArray = [];
-            let codesBase = ['THB', 'USD', 'AUD', 'HKD', 'CAD', 'NZD', 'SGD', 'EUR', 'HUF', 'CHF', 'GBP', 
-                            'UAH', 'JPY', 'CZK', 'DKK', 'ISK', 'NOK', 'SEK', 'HRK', 'RON', 
-                            'BGN', 'TRY', 'ILS', 'CLP', 'PHP', 'MXN', 'ZAR', 'BRL', 'MYR', 
-                            'IDR', 'INR', 'KRW', 'CNY', 'XDR']
+                            let codesArray = [];
+                            let codesBase = ['THB', 'USD', 'AUD', 'HKD', 'CAD', 'NZD', 'SGD', 'EUR', 'HUF', 'CHF', 'GBP', 
+                                            'UAH', 'JPY', 'CZK', 'DKK', 'ISK', 'NOK', 'SEK', 'HRK', 'RON', 
+                                            'BGN', 'TRY', 'ILS', 'CLP', 'PHP', 'MXN', 'ZAR', 'BRL', 'MYR', 
+                                            'IDR', 'INR', 'KRW', 'CNY', 'XDR']
 
-            for(let i = 0; i < response.body[0].rates.length; i++) {
-                codesArray.push(response.body[0].rates[i].code);
-            }
+                            for(let i = 0; i < response.body[0].rates.length; i++) {
+                                codesArray.push(response.body[0].rates[i].code);
+                            }
 
-            codesArray.forEach(code => {
-                expect(codesBase.includes(code)).to.eq(true);
-            });
-        });
+                            codesArray.forEach(code => {
+                                expect(codesBase.includes(code)).to.eq(true);
+                            });
+                        });
     });
 
     it('Testing exchange rate of currency BRL from the exchange rate table of type B', () => {
-        cy.request({
-            method: 'GET',
-            url: 'http://api.nbp.pl/api/exchangerates/rates/B/BRL/?format=json'
-        })
-        .then(response => {
-            expect(response.body).has.property('country', 'Brazylia');
-            expect(response.body).has.property('currency', 'real');
-            expect(response.body.table).to.eq('B');
-            expect(response.body.rates.length).to.be.equal(1);
-        });
+        api_functions.getRecordsFromTable(urlsTable.exchangeratesTableB)
+                        .then(response => {
+                            expect(response.body).has.property('country', 'Brazylia');
+                            expect(response.body).has.property('currency', 'real');
+                            expect(response.body.table).to.eq('B');
+                            expect(response.body.rates.length).to.be.equal(1);
+                        });
     });
 
     it('Testing the table of type A published today, if today`s table has not been published yet, the 404 error code is returned', () => {
-        cy.request({
-            method: 'GET',
-            failOnStatusCode: false,
-            url: 'http://api.nbp.pl/api/exchangerates/tables/a/today/'
-        })
-        .then(response => {
-            if(response.status == 200) {
-                expect(response.body[0].table).to.eq('A');
+        api_functions.getRecordsFromTableWithNoStatusFail(urlsTable.exchangeratesTableAToday)
+                        .then(response => {
+                            if(response.status == 200) {
+                                expect(response.body[0].table).to.eq('A');
 
-                let todayDate = new Date();
-                let formattedDate = todayDate.getFullYear() + '-' + ('0' + (todayDate.getMonth() + 1)).slice(-2) + '-' 
-                                    + ('0' + todayDate.getDate()).slice(-2);
+                                let todayDate = new Date();
+                                let formattedDate = todayDate.getFullYear() + '-' + ('0' + (todayDate.getMonth() + 1)).slice(-2) + '-' 
+                                                    + ('0' + todayDate.getDate()).slice(-2);
 
-                expect(response.body[0].effectiveDate).to.eq(formattedDate);
-                expect(response.body[0].rates.length).to.be.equal(34);
-            }
-            else {
-                expect(response.status).to.equal(404);
-            }
-        });
+                                expect(response.body[0].effectiveDate).to.eq(formattedDate);
+                                expect(response.body[0].rates.length).to.be.equal(34);
+                            }
+                            else {
+                                expect(response.status).to.equal(404);
+                            }
+                        });
     });
 
     it('Testing series of the last 10 quotations of GBP average exchange rate in the JSON', () => {
-        cy.request({
-            method: 'GET',
-            url: 'http://api.nbp.pl/api/exchangerates/rates/a/gbp/last/10/?format=json'
-        })
-        .then(response => {
-            expect(response.body.rates.length).to.eq(10);
-            expect(response.body.code).is.eq('GBP');
-        });
+        api_functions.getRecordsFromTable(urlsTable.exchangeratesTableAGBP10Days)
+                        .then(response => {
+                            expect(response.body.rates.length).to.eq(10);
+                            expect(response.body.code).is.eq('GBP');
+                        });
     });
 
     it('Testing series of GBP average exchange rates from 2012-01-01 to 2012-01-31', () => {
-        cy.request({
-            method: 'GET',
-            url: 'http://api.nbp.pl/api/exchangerates/rates/a/gbp/2012-01-01/2012-01-31/'
-        })
-        .then(response => {
-            expect(response.body.rates.length).to.eq(21);
-            expect(response.body.code).is.eq('GBP');
-            expect(response.body.rates[0].effectiveDate).is.eq('2012-01-02');
-            expect(response.body.rates[20].effectiveDate).is.eq('2012-01-31');
-        });
+        api_functions.getRecordsFromTable(urlsTable.exchangeratesTableAGBPDate)
+                        .then(response => {
+                            expect(response.body.rates.length).to.eq(21);
+                            expect(response.body.code).is.eq('GBP');
+                            expect(response.body.rates[0].effectiveDate).is.eq('2012-01-02');
+                            expect(response.body.rates[20].effectiveDate).is.eq('2012-01-31');
+                        });
     });
 
     it('Testing forbidden PUT request', () => {
-        cy.request({
-            method: 'PUT',
-            failOnStatusCode: false,
-            url: 'http://api.nbp.pl/api/exchangerates/rates/a/chf/'
-        })
-        .then(response => {
-            expect(response.status).is.eq(404);
-        });
+        const body = {
+            code: 'GBP'
+        }
+        api_functions.updateRecordInTableNoToken(urlsTable.exchangeratesTableACHF, body)
+                        .then(response => {
+                            expect(response.status).is.eq(404);
+                        });
     });
-
 });
