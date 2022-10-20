2a1b3c61132667f31dd15c803d493dda07fe4813
Katsiaryna Pazniak <Katsiaryna.Pazniak@itechart-group.com>
2022-10-20 11:20:23 +0200

    added reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/report',
+    overwrite: true,
+    html: false,
+    json: true
+  },
   projectId: '61p832',
   env: {
     bearerToken: '5a297804ccbed4506a99e50ee8248862f681173f23951f2e6078dde879f7e539'
   },
   e2e: {
+    setupNodeEvents(on) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    }
 
   },
 });
